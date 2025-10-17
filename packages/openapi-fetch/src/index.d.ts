6dfe50d675ba75da5a5d37814d75a15c86867d20
ws-rush <wusaby.rush@zohomail.com>
2025-10-17 10:50:54 +0300

    fix: lint errors

--- packages/openapi-fetch/src/index.d.ts
+++ packages/openapi-fetch/src/index.d.ts
@@ -160,10 +160,7 @@ type MiddlewareOnError = (
 ) => void | Response | Error | Promise<void | Response | Error>;
 
 export type Middleware =
-  | ((
-      context: MiddlewareCallbackParams,
-      next: () => Promise<Response>,
-    ) => Promise<Response>)
+  | ((context: MiddlewareCallbackParams, next: () => Promise<Response>) => Promise<Response>)
   | {
       onRequest: MiddlewareOnRequest;
       onResponse?: MiddlewareOnResponse;
--- packages/openapi-fetch/src/index.js
+++ packages/openapi-fetch/src/index.js
@@ -4,7 +4,7 @@ const PATH_PARAM_RE = /\{[^{}]+\}/g;
 const supportsRequestInitExt = () => {
   return (
     typeof process === "object" &&
-    Number.parseInt(process?.versions?.node?.substring(0, 2)) >= 18 &&
+    Number.parseInt(process?.versions?.node?.substring(0, 2), 10) >= 18 &&
     process.versions.undici
   );
 };
@@ -111,9 +111,9 @@ export default function createClient(clientOptions) {
       headers: finalHeaders,
     };
 
-    let id;
-    let options;
-    let request = new Request(
+    let _id;
+    let _options;
+    const request = new Request(
       createFinalURL(schemaPath, { baseUrl: finalBaseUrl, params, querySerializer }),
       requestInit,
     );
